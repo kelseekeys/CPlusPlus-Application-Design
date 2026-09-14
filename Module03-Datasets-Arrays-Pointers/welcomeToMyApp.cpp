3c6466f8e569f9fbcecc5269e63cebc652be3d7e
KELSEE KEYS <kelseekeys@gmail.com>
2026-09-14 15:40:19 -0700

    add file
    adding file for module 3

---
+++ Module03-Datasets-Arrays-Pointers/welcomeToMyApp.cpp
@@ -0,0 +1,30 @@
+// Name: Kelsee Keys
+// Course: CIS 25
+// Date: 09/12/2026
+// Assignment: Variables, Cin, Cout / Module 3
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Main C++ variable types
+
+    int age = 28;                    // whole number
+    double birthday = 12.31;         // decimal number
+    float height = 5.6;              // decimal number
+    char initial = 'K';              // single character
+    bool pets = true;                // true or false
+    string name = "Kelsee";          // text
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Birthday: " << birthday << endl;
+    cout << "Height: " << height << endl;
+    cout << "Initial: " << initial << endl;
+    cout << "Pets: " << pets << endl;
+
+    return 0;
+}
